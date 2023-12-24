836a176c5558fde6fa5f000d5d865c0633b283ac
primextopx <148803510+primextopx@users.noreply.github.com>
2023-12-24 14:07:36 +0100

    Add files via upload

---
+++ Nebula raider.bat
@@ -0,0 +1,9 @@
+@echo off
+pip install logging
+pip install socket
+pip install requests
+pip install dhooks
+pip install pynput
+pip install pycryptodome
+title Nebula Raider
+python "Nebula Tkn Spammer.py"
